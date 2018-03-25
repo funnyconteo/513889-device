bb5c3b6f133262d6d7393732dbe10a7ec8667e3a
funnyconteo <funnyconteo@gmail.com>
2018-03-25 22:14:01 +0400

    16
    вторая страница

--- js/script.js
+++ js/script.js
@@ -1,5 +1,1 @@
-slideDelivery.addEventListener('click', function () {
-  console.log('Отображается слайд ДОСТАВКА');
-  delivery.classList.add('slider-service__button--current');
-  warranty.classList.remove('slider-service__button--current');
-  credit.classList.remove('slider-service__button--current');
+
