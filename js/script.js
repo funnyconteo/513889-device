d3a3644f2105b7e1f0d29bdc481d8b38faa503af
funnyconteo <funnyconteo@gmail.com>
2018-03-26 01:56:02 +0400

    17
    модалки

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 
+var btnopen = document.getElementsByClassName('modal-btn');
+var mapopen = document.getElementsByClassName('open-map');
+var btnclose = document.getElementsByClassName('modal-close');
+var write = document.getElementsByClassName('write-us');
+var map = document.getElementsByClassName('map');
+btnopen[0].onclick = function() {
+  write[0].classList.add('visual');
+}
+mapopen[0].onclick = function() {
+  map[0].classList.add('visual');
+}
+btnclose[0].onclick = function() {
+  write[0].classList.remove('visual');
+}
+btnclose[1].onclick = function() {
+  map[0].classList.remove('visual');
+}
